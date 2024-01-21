0fb587078dc8333bd093adc8e1f48e788874af65
Bone <bonedrums@gmail.com>
2024-01-21 13:50:55 -0500

    Add files via upload

---
+++ AddOnSkins.lua
@@ -0,0 +1,22 @@
+
+AddOnSkinsDB = {
+	["profileKeys"] = {
+		["Gourgamell - Benediction"] = "GhoulUI",
+	},
+	["profiles"] = {
+		["GhoulUI"] = {
+			["Font"] = "Oswald",
+			["EmbedSystemMessage"] = false,
+			["EmbedBackdropTransparent"] = false,
+			["EmbedFrameStrata"] = "4-HIGH",
+			["DBMFont"] = "Oswald",
+			["EmbedFrameLevel"] = 100,
+			["EmbedLeftWidth"] = 230,
+			["FontFlag"] = "SHADOWOUTLINE",
+			["EmbedSystemDual"] = true,
+			["EmbedBackdrop"] = false,
+		},
+	},
+}
+AddOnSkinsDS = {
+}
